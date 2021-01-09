eb537b6c9513bdb94cc119b9d2be534ee0954eca
enjay27 <kdkm2002@gmail.com>
2021-01-09 13:46:46 +0900

    item 21

---
+++ src/interfaceEx/Editable.java
@@ -0,0 +1,12 @@
+package interfaceEx;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public interface Editable {
+
+
+//    default void edit() {
+//    }
+}
---
+++ src/interfaceEx/ThreadEx.java
@@ -0,0 +1,31 @@
+package interfaceEx;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ThreadEx implements Runnable {
+
+    List<Integer> synchronizedList = Collections.synchronizedList(new ArrayList<Integer>());
+
+    @Override
+    public void run() {
+        while (true) {
+            for (int i = 0; i < 10; i++) {
+                synchronizedList.add(i * 2);
+            }
+            System.out.println(Thread.currentThread().getName() + " add Numbers" + synchronizedList.toString());
+
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            if(synchronizedList.removeIf(n -> (n % 2 == 0)))
+                System.out.println(Thread.currentThread().getName() + " remove Numbers"+ synchronizedList.toString());
+
+        }
+
+    }
+}
