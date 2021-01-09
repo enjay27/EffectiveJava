0d93cb6041e6dc6564dfbbef88aa4819ece0775f
enjay27 <kdkm2002@gmail.com>
2021-01-09 13:51:35 +0900

    item 21

---
+++ src/interfaceEx/Item21.java
@@ -0,0 +1,20 @@
+package interfaceEx;
+
+public class Item21 {
+    public static void main(String[] args) {
+        ThreadEx threadExA = new ThreadEx();
+        ThreadEx threadExB = new ThreadEx();
+        ThreadEx threadExC = new ThreadEx();
+        ThreadEx threadExD = new ThreadEx();
+
+        Thread threadA = new Thread(threadExA, "A");
+        Thread threadB = new Thread(threadExA, "B");
+        Thread threadC = new Thread(threadExA, "C");
+        Thread threadD = new Thread(threadExA, "D");
+
+        threadA.start();
+        threadB.start();
+        threadC.start();
+        threadD.start();
+    }
+}
